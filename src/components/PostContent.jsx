baf9f78c09b34387f525693c3ae63a877ff4eaf9
acskii <andrew.sameh.06@gmail.com>
2025-04-16 13:41:27 +0200

    style: fix dark mode
    KNOWN ISSUES:
        post markdown doesn't support light mode.

--- src/components/PostContent.jsx
+++ src/components/PostContent.jsx
@@ -1,28 +1,28 @@
 import { Navigate } from "react-router-dom";
-//import MarkdownRenderer from "./MarkdownRenderer";
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import ReactMarkdown from "react-markdown";
 import remarkGfm from 'remark-gfm';
-import rehypeSlug from 'rehype-slug';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+// import rehypeSlug from 'rehype-slug';
+// import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import 'github-markdown-css';
 import './PostContent.css';
+//import MarkdownRenderer from "./MarkdownRenderer";
 
 function PostContent({content}) {
     if (content == null) {
         return <Navigate to="/404" replace/>;
     } else {
         return (
-        <div className="markdown-body p-4">
-            <ReactMarkdown
+        <div className="markdown-body p-4 w-full">
+        <ReactMarkdown
             remarkPlugins={[remarkGfm]}
-            rehypePlugins={[rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'wrap' }]]}
+            //rehypePlugins={[rehypeSlug, [rehypeAutolinkHeadings, {'behavior': 'after'}]]}
             components={{
                 code({ node, inline, className, children, ...props }) {
                 const match = /language-(\w+)/.exec(className || '');
                 return !inline && match ? (
-                    <SyntaxHighlighter language={match[1]} style={oneDark} PreTag="div" {...props}>
+                    <SyntaxHighlighter language={match[1]} style={dracula} PreTag="div" {...props}>
                     {String(children).replace(/\n$/, '')}
                     </SyntaxHighlighter>
                 ) : (
@@ -33,7 +33,7 @@ function PostContent({content}) {
                 },
             }}>
             {content}
-            </ReactMarkdown> 
+        </ReactMarkdown>
         </div>
         );
     }
