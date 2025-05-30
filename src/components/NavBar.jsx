4d2ce236e3a9d153ccea7b8a9d738736547cf11b
acskii <andrew.sameh.06@gmail.com>
2025-05-30 20:41:31 +0300

    style: changed styling for navbar and footer for all pages.
    - theme control is now more reliable
    - on mobile screens, navbar options show up in a drawer menu
    - footer contains GitHub page link for now

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,35 +1,59 @@
 import { Link } from "react-router-dom";
-import links from "../links.json";
+import { Burger, Group, Box, ScrollArea, Drawer, Divider } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import ThemeControl from "./ThemeControl";
-import DropdownMenu from "./DropdownMenu";
+import './NavBar.css';
+
+const links = [
+    {link: '/home', label: 'Home'},
+    {link: '/posts', label: 'Posts'},
+    {link: '/about', label: 'About'}
+];
 
 function NavBar() {
+    const [opened, { toggle, close }] = useDisclosure(false);
+    const items = links.map((link) => (<Link key={link.label} to={link.link} onClick={close} className="link">
+      {link.label}
+    </Link>));
     return (
-        <div id="navbar" className="navbar sticky top-0 light:bg-accent bg-neutral flex flex-row gap-2 z-50">
-            <div id="navbar-home" className="navbar-start">
-                <Link to="/">
-                    <button className="btn btn-ghost text-xl font-bold p-2">Blog</button>
-                </Link>
-                <a href={links.github}>
-                    <span className="text-sm text-nowrap">| acskii</span>
-                </a>
-            </div>
-            <div id="navbar-tabs" className="hidden lg:flex navbar-center flex-row gap-3">
-                <Link to="/home">
-                    <button className="btn btn-soft btn-secondary md:text-lg sm:text-sm font-bold">Home</button>
-                </Link>
-                <Link to="/posts">
-                    <button className="btn btn-soft btn-primary md:text-lg sm:text-sm font-bold">Posts</button>
-                </Link>
-                <Link to="/about">
-                    <button className="btn btn-soft btn-secondary md:text-lg sm:text-sm font-bold">About</button>
-                </Link>
-            </div>
-            <div className="navbar-end">
-                <ThemeControl />
-                <DropdownMenu />
-            </div>
-        </div>
+        <Box>
+            <header className="header">
+                <div className="inner">
+                    <Group>
+                        <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm"/>
+                        <h2 className="text-xl font-bold">Blog</h2>
+                        <Link to="https://www.github.com/acskii/">
+                            <span className="text-sm text-nowrap">| acskii</span>
+                        </Link>
+                    </Group>
+
+                    <Group>
+                        <Group ml={50} gap={5} className="links" visibleFrom="sm">
+                            {items}
+                        </Group>
+                        <ThemeControl />
+                    </Group>
+                </div>
+            </header>
+             <Drawer
+                opened={opened}
+                onClose={close}
+                size="100%"
+                padding="md"
+                title="Navigation"
+                hiddenFrom="sm"
+                zIndex={1000000}
+            >
+                <ScrollArea h={`calc(100vh - 60px)`} mx="-md">
+                    {items[0]}
+                    <Divider my="sm" />
+                    {items[1]}
+                    <Divider my="sm" />
+                    {items[2]}
+                    <Divider my="sm" />
+                </ScrollArea>
+            </Drawer>
+        </Box>
     );
 }
 
